3c2a523aa2c060c40440d02628b1b01b62fd2e2a
saroj7 <sarojsht377@gmail.com>
2021-01-24 14:02:05 +0545

    Token Contract

---
+++ closure.py
@@ -0,0 +1,13 @@
+{
+    "jsonrpc": "2.0",
+    "method": "icx_call",
+    "params": {
+        "from": "hxe7af5fcfd8dfc67530a01a0e403882687528dfcb",
+        "to": "cx060eb55c32ef5087d112badbbb50b0315c1167f3",
+        "dataType": "call",
+        "data": {
+            "method": "hello"
+        }
+    },
+    "id": 1
+}
